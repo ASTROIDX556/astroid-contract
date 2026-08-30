2cb857bfb4ce88eddce03b2345c4a65f70491098
hayzed <abrcrmb04@outlook.com>
2026-08-30 11:09:49 +0100

    chore: remove scratch scripts

--- test.py
+++
@@ -1,6 +0,0 @@
-import re
-with open("shared/src/errors.rs", "r") as f:
-    c = f.read()
-c = c.replace("PolicyCategoryRestricted = 25,", "PolicyCategoryRestricted = 25,\n    Test1 = 26,\n    Test2 = 27,\n    Test3 = 28,")
-with open("shared/src/errors.rs", "w") as f:
-    f.write(c)
