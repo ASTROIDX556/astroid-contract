84996daf9068dad2713f66b41139d9fca319490c
fatiah <shehufatiah03@gmail.com>
2026-08-30 11:10:09 +0100

    chore: remove scratch scripts

--- fix_policy_brace.py
+++
@@ -1,5 +0,0 @@
-with open("contracts/policy/src/lib.rs", "r") as f:
-    c = f.read()
-c = c.replace("mod test;", "}\nmod test;")
-with open("contracts/policy/src/lib.rs", "w") as f:
-    f.write(c)
--- patch_policy_tests.py
+++
@@ -1,8 +0,0 @@
-with open("contracts/policy/src/test.rs", "r") as f:
-    text = f.read()
-
-if "use astroid_shared::errors::Error;" not in text:
-    text = text.replace("use soroban_sdk::{", "use astroid_shared::errors::Error;\nuse soroban_sdk::{")
-
-with open("contracts/policy/src/test.rs", "w") as f:
-    f.write(text)
